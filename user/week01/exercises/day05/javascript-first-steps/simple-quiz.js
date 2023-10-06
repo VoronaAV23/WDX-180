534d5ee8ef2e75ec5bb5ea652a4474a86711d622
Anna Vorona <19anna_crow94@ukr.net>
2023-10-06 17:43:46 +0300

    javascript-simple-quiz

---
+++ user/week01/exercises/day05/javascript-first-steps/simple-quiz.js
@@ -0,0 +1,12 @@
+let userInput = prompt("Who created JavaScript?\nA. Brendan Eich\nB. Bill Gates\nC. Mark Zuckerberg").toUpperCase();
+
+
+if (userInput === "A") {
+    alert("Correct! Not only did he create JS, the prototype of the language was ready in 10 days!");
+} else if (userInput === "B") {
+    alert("Nope. He led the development of the Windows Operating System.");
+} else if (userInput === "C") {
+    alert("Nope. He just came up with the idea for a social network.");
+} else {
+    alert ("Invalid answer")
+}
